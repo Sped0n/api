9c98e4b9b7c4f532c358cf5dc2df740965426d63
Spedon <RealSped0n@outlook.com>
2023-01-29 14:23:03 +0800

    naming bug fix

--- djangoapi/asgi.py
+++ djangoapi/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "arc_pls_api.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangoapi.settings")
 
 application = get_asgi_application()
--- djangoapi/wsgi.py
+++ djangoapi/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for arc_pls_api project.
+WSGI config for djangoapi project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "arc_pls_api.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangoapi.settings")
 
 application = get_wsgi_application()
