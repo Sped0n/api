38c723641e5f15afa84e4908685c098ff4dfb019
Spedon <RealSped0n@outlook.com>
2023-01-30 02:26:44 +0800

    register the models

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from api.models import MetricsCache
+from api.models import ErrorLogs
+
+admin.site.register(MetricsCache)
+admin.site.register(ErrorLogs)
