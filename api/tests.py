10cc663b12dd11be1b76a2476a88e5e28f5bcf42
Spedon <RealSped0n@outlook.com>
2023-01-30 14:37:58 +0800

    add test procedure

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.test import Client
+import os
+
+class ApiTestCase(TestCase):
+
+    def setUp(self):
+        c = Client()
+        self.resp = c.get('/arc_metric/')
+
+    def test_status_code(self):
+        self.assertIs(self.resp.status_code, 200)
+        print(self.resp.content)
+
+    def test_content_type(self):
+        self.assertEqual(self.resp['content-type'], 'application/json')
