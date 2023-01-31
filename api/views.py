c155e0aea731e9b114ae21152770d4cdce81babe
Spedon <RealSped0n@outlook.com>
2023-01-31 18:54:09 +0800

    cors test

--- api/views.py
+++ api/views.py
@@ -91,7 +91,6 @@ def arc_metric_api(request):
         metrics_json = fetch_latest_data()
     response = JsonResponse(metrics_json)
     response["Access-Control-Allow-Origin"] = "*"
-    response["Access-Control-Allow-Methods"] = "GET"
     response["Access-Control-Max-Age"] = "1000"
     response["Access-Control-Allow-Headers"] = "X-Requested-With, Content-Type"
     return response
