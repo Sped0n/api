e0220ac57e22e6ccb3b226d082dfeabcea1a5092
Spedon <RealSped0n@outlook.com>
2023-01-31 13:16:59 +0800

    add import local_setting.py to secure api key

--- djangoapi/settings.py
+++ djangoapi/settings.py
@@ -125,3 +125,9 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+try:
+    from local_settings import set_api_key
+    set_api_key()
+except ImportError:
+    pass  # No local_settings file
