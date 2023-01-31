b17f03ae118c03c6599753449c3768d39aa58363
Spedon <RealSped0n@outlook.com>
2023-01-31 13:17:25 +0800

    add json match test case

--- api/tests.py
+++ api/tests.py
@@ -2,17 +2,20 @@ from django.test import TestCase
 
 # Create your tests here.
 from django.test import Client
-import os
+
 
 class ApiTestCase(TestCase):
 
     def setUp(self):
         c = Client()
-        self.resp = c.get('/arc_metric/')
+        self.resp = c.get('/arc_metric/', format='json')
 
     def test_status_code(self):
         self.assertIs(self.resp.status_code, 200)
-        print(self.resp.content)
 
     def test_content_type(self):
         self.assertEqual(self.resp['content-type'], 'application/json')
+
+    def test_return_json(self):
+        self.assertJSONEqual(self.resp.content.decode('utf-8'),
+                             {"bounce_rate": 0.0, "pageviews": 1, "visit_duration": 0.0, "visitors": 1})
