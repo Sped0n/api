b139ebadd1eaa00e434dd2935257e14728f8ce41
Sped0n <realsped0n@outlook.com>
2023-09-03 20:56:31 +0800

    test(api): update API test cases
    - Updated the URL format in the `setUp` method to use double quotes instead of single quotes.
    - Added a type hint to ignore the type error for `self.assertIs` and `self.assertEqual`.
    - Modified the expected value in the `test_content_type` method to use double quotes instead of single quotes.
    - Updated the expected JSON response in the `test_return_json` method to only include the "pageviews" key.

--- api/tests.py
+++ api/tests.py
@@ -5,17 +5,18 @@ from django.test import Client
 
 
 class ApiTestCase(TestCase):
-
     def setUp(self):
         c = Client()
-        self.resp = c.get('/arc_metric/', format='json')
+        self.resp = c.get("/arc_metric/", format="json")
 
     def test_status_code(self):
-        self.assertIs(self.resp.status_code, 200)
+        self.assertIs(self.resp.status_code, 200)  # type: ignore
 
     def test_content_type(self):
-        self.assertEqual(self.resp['content-type'], 'application/json')
+        self.assertEqual(self.resp["content-type"], "application/json")  # type: ignore
 
     def test_return_json(self):
-        self.assertJSONEqual(self.resp.content.decode('utf-8'),
-                             {"bounce_rate": 0.0, "pageviews": 1, "visit_duration": 0.0, "visitors": 1})
+        self.assertJSONEqual(
+            self.resp.content.decode("utf-8"),  # type: ignore
+            {"pageviews": 1},
+        )
